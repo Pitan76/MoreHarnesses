8c0699fc0111eb5cfb649f0b7e3b0edaefe424a5
PTOM76 <58260965+PTOM76@users.noreply.github.com>
2025-10-20 22:55:09 +0900

    Update game versions and refactor equipment asset registration to use new mcpitanlib API

--- common/src/main/java/net/pitan76/moreharnesses/item/EquipmentAssets.java
+++ common/src/main/java/net/pitan76/moreharnesses/item/EquipmentAssets.java
@@ -1,19 +1,17 @@
 package net.pitan76.moreharnesses.item;
 
-import net.minecraft.item.equipment.EquipmentAsset;
-import net.minecraft.registry.RegistryKey;
-import net.pitan76.moreharnesses.util.EquipmentAssetUtil;
+import net.pitan76.mcpitanlib.api.item.equipment.CompatEquipmentAsset;
 
 import static net.pitan76.moreharnesses.MoreHarnesses._id;
 
 public class EquipmentAssets {
 
-    public static RegistryKey<EquipmentAsset> COPPER_HARNESS = EquipmentAssetUtil.register(_id("copper_harness"));
-    public static RegistryKey<EquipmentAsset> IRON_HARNESS = EquipmentAssetUtil.register(_id("iron_harness"));
-    public static RegistryKey<EquipmentAsset> GOLD_HARNESS = EquipmentAssetUtil.register(_id("gold_harness"));
-    public static RegistryKey<EquipmentAsset> DIAMOND_HARNESS = EquipmentAssetUtil.register(_id("diamond_harness"));
-    public static RegistryKey<EquipmentAsset> EMERALD_HARNESS = EquipmentAssetUtil.register(_id("emerald_harness"));
-    public static RegistryKey<EquipmentAsset> NETHERITE_HARNESS = EquipmentAssetUtil.register(_id("netherite_harness"));
+    public static CompatEquipmentAsset COPPER_HARNESS = CompatEquipmentAsset.of(_id("copper_harness"));
+    public static CompatEquipmentAsset IRON_HARNESS = CompatEquipmentAsset.of(_id("iron_harness"));
+    public static CompatEquipmentAsset GOLD_HARNESS = CompatEquipmentAsset.of(_id("gold_harness"));
+    public static CompatEquipmentAsset DIAMOND_HARNESS = CompatEquipmentAsset.of(_id("diamond_harness"));
+    public static CompatEquipmentAsset EMERALD_HARNESS = CompatEquipmentAsset.of(_id("emerald_harness"));
+    public static CompatEquipmentAsset NETHERITE_HARNESS = CompatEquipmentAsset.of(_id("netherite_harness"));
 
     public static void init() {
 
--- common/src/main/java/net/pitan76/moreharnesses/item/HarnessItem.java
+++ common/src/main/java/net/pitan76/moreharnesses/item/HarnessItem.java
@@ -1,38 +1,37 @@
 package net.pitan76.moreharnesses.item;
 
-import net.minecraft.component.DataComponentTypes;
-import net.minecraft.component.type.AttributeModifierSlot;
-import net.minecraft.component.type.AttributeModifiersComponent;
-import net.minecraft.entity.attribute.EntityAttributeModifier;
-import net.minecraft.entity.attribute.EntityAttributeModifier.Operation;
-import net.minecraft.entity.attribute.EntityAttributes;
-import net.minecraft.item.Item;
-import net.minecraft.item.equipment.EquipmentAsset;
-import net.minecraft.registry.RegistryKey;
+import net.pitan76.mcpitanlib.api.entity.attribute.*;
+import net.pitan76.mcpitanlib.api.item.ArmorEquipmentType;
+import net.pitan76.mcpitanlib.api.item.equipment.CompatEquipmentAsset;
+import net.pitan76.mcpitanlib.api.item.equipment.EquippableComponentBuilder;
+import net.pitan76.mcpitanlib.api.item.v2.CompatItem;
 import net.pitan76.mcpitanlib.api.item.v2.CompatibleItemSettings;
+import net.pitan76.mcpitanlib.api.sound.CompatSoundEvents;
 import net.pitan76.moreharnesses.MoreHarnesses;
-import net.pitan76.moreharnesses.util.EquipmentAssetUtil;
 
-public class HarnessItem extends Item {
+public class HarnessItem extends CompatItem {
 
     private final double additionalSpeed;
 
-    public HarnessItem(CompatibleItemSettings settings, RegistryKey<EquipmentAsset> key, double additionalSpeed, double additionalArmor) {
-        super(settings.build()
-                .component(DataComponentTypes.EQUIPPABLE, EquipmentAssetUtil.ofHarness(key))
-                .component(DataComponentTypes.ATTRIBUTE_MODIFIERS,
-                        AttributeModifiersComponent.builder()
-                                .add(EntityAttributes.ARMOR,
-                                        new EntityAttributeModifier(MoreHarnesses._id("armor").toMinecraft(), additionalArmor, Operation.ADD_VALUE)
-                                        , AttributeModifierSlot.BODY)
-                                .build()
+    public HarnessItem(CompatibleItemSettings settings, CompatEquipmentAsset asset, double additionalSpeed, double additionalArmor) {
+        super(settings.equipable(new EquippableComponentBuilder(ArmorEquipmentType.BODY)
+                        .equipSound(CompatSoundEvents.ENTITY_HAPPY_GHAST_EQUIP)
+                        .model(asset)
+                        .allowedEntities("can_equip_harness")
+                        .equipOnInteract(true)
+                        .canBeSheared(true)
+                        .shearingSound(CompatSoundEvents.ENTITY_HAPPY_GHAST_UNEQUIP)
+        ).attributeModifiers(
+                new AttributeModifiersComponentBuilder().add(CompatEntityAttributes.ARMOR,
+                        CompatEntityAttributeModifier.of(MoreHarnesses._id("armor"), additionalArmor, AttrModifierOperation.ADD),
+                        CompatAttributeModifierSlot.BODY)
                 )
         );
         this.additionalSpeed = additionalSpeed;
     }
 
-    public HarnessItem(CompatibleItemSettings settings, RegistryKey<EquipmentAsset> key, double additionalSpeed) {
-        this(settings, key, additionalSpeed, 0.0);
+    public HarnessItem(CompatibleItemSettings settings, CompatEquipmentAsset asset, double additionalSpeed) {
+        this(settings, asset, additionalSpeed, 0.0);
     }
 
     public double getAdditionalSpeed() {
